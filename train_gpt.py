dbdad927d14703ecbeccdf83ace16246fb4ac312
saienduri <77521230+saienduri@users.noreply.github.com>
2025-01-13 22:34:24 -0800

    amd

--- train_gpt.py
+++ train_gpt.py
@@ -425,7 +425,7 @@ print0('='*100)
 # log information about the hardware/software environment this is running on
 print0(f'Running Python {sys.version}')
 print0(f'Running PyTorch {torch.version.__version__} compiled for CUDA {torch.version.cuda}')
-print0(subprocess.run(['nvidia-smi'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True).stdout)
+print0(subprocess.run(['rocm-smi'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True).stdout)
 print0('='*100)
 
 # load data
